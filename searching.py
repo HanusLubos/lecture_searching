c6fece13c9b7214329c3306706a2b833aa2f82cf
HanusLubos <lubos.hanus6@gmail.com>
2024-04-10 09:31:13 +0200

    linear_search

--- searching.py
+++ searching.py
@@ -23,10 +23,23 @@ def read_data(file_name, field):
     return data[field]
 
 
+def linear_search(nums, target_num):
+    positions = []
+    count = 0
+    for idx in range(len(nums)):
+        if nums[idx] == target_num:
+            positions.append(idx)
+            count += 1
+
+    my_dict = {"positions": positions, "count": count}
+    return my_dict
 
 def main():
     unordered_numbers = read_data("sequential.json", "unordered_numbers")
     print(unordered_numbers)
+
+    occurance_of_num = linear_search(unordered_numbers, 9)
+    print(occurance_of_num)
 
 
 
