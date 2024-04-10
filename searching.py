f85688a0bdcfc2a3bf8e06c9095a50b109a89af9
HanusLubos <lubos.hanus6@gmail.com>
2024-04-10 10:04:05 +0200

    pattern_search

--- searching.py
+++ searching.py
@@ -34,12 +34,30 @@ def linear_search(nums, target_num):
     my_dict = {"positions": positions, "count": count}
     return my_dict
 
+def pattern_search(sequence, vzor):
+    idxs = set()
+    vzor_delka = len(vzor)
+    for idx in range(len(sequence)):
+        tesovaci_str = sequence[idx:idx + vzor_delka]
+        if tesovaci_str == vzor:
+            idx = idx + int(vzor_delka/2)
+            idxs.add(idx)
+    return idxs
+
+
 def main():
     unordered_numbers = read_data("sequential.json", "unordered_numbers")
-    print(unordered_numbers)
+    # print(unordered_numbers)
+
+    letter_sequence = read_data("sequential.json", "dna_sequence")
+
+    occurance_let_seq = pattern_search(letter_sequence, "GTA")
+    print(occurance_let_seq)
 
     occurance_of_num = linear_search(unordered_numbers, 9)
-    print(occurance_of_num)
+    # print(occurance_of_num)
+
+
 
 
 
